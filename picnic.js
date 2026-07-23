7580323375406f2b4f98c9bcd7eb339dd10b6b44
Muhammad Anas <peerzadamuhammadanash804@gmail.com>
2026-07-23 08:05:24 +0500

    Sahir Mane Kuch Acha Kr Lia Shayad

---
+++ picnic.js
@@ -0,0 +1,21 @@
+const hoverBox = document.getElementById('hover-info');
+const hoverImg = document.getElementById('hover-img');
+const hoverText = document.getElementById('hover-text');
+const gameItems = document.querySelectorAll('.game-list li');
+
+gameItems.forEach(item => {
+    item.addEventListener('mouseenter', (e) => {
+        hoverImg.src = item.getAttribute('data-img');
+        hoverText.innerText = item.getAttribute('data-detail');
+        hoverBox.style.display = 'flex';
+    });
+
+    item.addEventListener('mousemove', (e) => {
+        hoverBox.style.left = e.pageX + 20 + 'px'; 
+        hoverBox.style.top = e.pageY + 20 + 'px';
+    });
+
+    item.addEventListener('mouseleave', () => {
+        hoverBox.style.display = 'none';
+    });
+});
